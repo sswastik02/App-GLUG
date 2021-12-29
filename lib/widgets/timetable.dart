ba6c48cc7affa478071573d15938ee425f158355
sswastik02 <sswastik02@gmail.com>
2021-12-30 02:07:48 +0530

    Fixes: 	1. Fixed red screen in timetable Features: 	1. Display sem after year

--- lib/widgets/timetable.dart
+++ lib/widgets/timetable.dart
@@ -5,94 +5,116 @@ import 'package:dynamic_themes/dynamic_themes.dart';
 import 'package:glug_app/resources/routine_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-String section; int year;
+String section;
+int year;
+
 class TimeTable extends StatefulWidget {
-
-
-
-   TimeTable(String x, int y){
-     section=x; year=y;
-   }
+  TimeTable(String x, int y) {
+    section = x;
+    year = y;
+  }
 
   @override
   _TimeTableState createState() => _TimeTableState();
 }
 
-
-
 class _TimeTableState extends State<TimeTable> {
+  var color = {};
+  dynamic data = {};
 
-  var color={};dynamic data={};
-
-  Future getData()async{
+  Future getData() async {
     CollectionReference timetable =
-    FirebaseFirestore.instance.collection('timetable');
+        FirebaseFirestore.instance.collection('timetable');
     QuerySnapshot querySnapshot = await timetable.get();
     List<QueryDocumentSnapshot> lqds = querySnapshot.docs;
-    DocumentSnapshot documentSnapshot = await timetable.doc(lqds[year-1].id).get();
+    DocumentSnapshot documentSnapshot =
+        await timetable.doc(lqds[year - 1].id).get();
     setState(() {
       data = documentSnapshot.data();
     });
-
   }
-  void colorSc()async{
+
+  void colorSc() {
     RoutineData routineData = RoutineData();
     var set = <String>{};
-    for(var i = 0;i<11;i++){
-      for( var j =0;j<6;j++){
-        if(data[section]["${i}"][j]!="" && data[section]["${i}"][j].toString().length==5)
-        set.add(data[section]["${i}"][j].toString());
+    for (var i = 0; i < 11; i++) {
+      for (var j = 0; j < 6; j++) {
+        if (data[section]["${i}"][j] != "" &&
+            data[section]["${i}"][j].toString().length == 5)
+          set.add(data[section]["${i}"][j].toString());
       }
     }
     // print(color);
-    int c=1;
+    int c = 1;
     set.forEach((element) {
       setState(() {
-        color[element]=routineData.colorScheme[c];
+        color[element] = routineData.colorScheme[c];
         c++;
       });
     });
+  }
 
-
+  Future<int> allFutureCalls() async {
+    await getData();
+    colorSc();
+    return 1;
   }
 
   @override
   Widget build(BuildContext context) {
-  getData();
-  colorSc();
-    return (data==null || color==null)?Center(child: CircularProgressIndicator()):
-  Padding(
-      padding: const EdgeInsets.fromLTRB(9, 0, 9, 7),
-      child: Container(
-         child:  GridView.count(
-          crossAxisCount: 6,
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          children: List.generate(66, (index) {
-        return Padding(
-          padding: const EdgeInsets.all(3),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color:(data[section]["${(index/6).toInt()}"][index%6]!=null)?color[data[section]["${(index/6).toInt()}"][index%6]]:DynamicTheme.of(context).themeId==1 ? Colors.white : Colors.black,
-            ),
-           child: Center(child: Transform(
-             alignment: FractionalOffset.center,
-             transform: new Matrix4.identity()
-               ..rotateZ(0 * 3.1415927 / 180),
-             child: Text("${data[section]["${(index/6).toInt()}"][index%6]}",
-             style: TextStyle(
-               fontSize: MediaQuery.of(context).size.width * 0.046,
-               fontWeight: FontWeight.w600
-             ),
-             ),
-           )),
-        )
-        );
-      },
-         )
-         )
-      ),
-    );
+    return FutureBuilder<int>(
+        future: allFutureCalls(),
+        builder: (context, snapshot) {
+          return (!snapshot.hasData)
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Padding(
+                  padding: const EdgeInsets.fromLTRB(9, 0, 9, 7),
+                  child: Container(
+                      child: GridView.count(
+                          crossAxisCount: 6,
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          children: List.generate(
+                            66,
+                            (index) {
+                              return Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: (data[section]
+                                                      ["${(index / 6).toInt()}"]
+                                                  [index % 6] !=
+                                              null)
+                                          ? color[data[section]
+                                                  ["${(index / 6).toInt()}"]
+                                              [index % 6]]
+                                          : DynamicTheme.of(context).themeId ==
+                                                  1
+                                              ? Colors.white
+                                              : Colors.black,
+                                    ),
+                                    child: Center(
+                                        child: Transform(
+                                      alignment: FractionalOffset.center,
+                                      transform: new Matrix4.identity()
+                                        ..rotateZ(0 * 3.1415927 / 180),
+                                      child: Text(
+                                        "${data[section]["${(index / 6).toInt()}"][index % 6]}",
+                                        style: TextStyle(
+                                            fontSize: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.046,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    )),
+                                  ));
+                            },
+                          ))),
+                );
+        });
   }
 }
